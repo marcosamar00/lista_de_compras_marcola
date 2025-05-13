12185d9170e0bdb421cded0dd9fa7604e7a8c545
Marcos V. Amaro <160757978+marcosamar00@users.noreply.github.com>
2025-05-13 09:18:02 -0300

    q

--- supabseClient.js
+++ supabseClient.js
@@ -3,6 +3,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://wqrvadycionozczmqgoj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxcnZhZHljaW9ub3pjem1xZ29qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTUzOTIsImV4cCI6MjA2MjA3MTM5Mn0.mMVAn7kJQH8bLhijfug2E0lMvpjRkKRE7CC29IFNNt4'
+  'https://hzxlcmqbbsntoaopdvzb.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6eGxjbXFiYnNudG9hb3BkdnpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU1MDAsImV4cCI6MjA2MjExMTUwMH0.1ETknQzoodZ0QoOIJ4WJARfD2OnTiCF3ZmaDVlGNa9g'
 )
